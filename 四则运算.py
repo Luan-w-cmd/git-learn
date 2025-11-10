2911c19f2e98419d4f4ed93ea74a5b1f1ea31879
weixuan <3332702142@qq.com>
2025-11-10 23:56:52 +0800

    修改了四则运算逻辑

--- 四则运算.py
+++ 四则运算.py
@@ -1,14 +1,28 @@
-a=int(input("请输入第一个数字："))
-b=input("请输入运算符号：")
-c=int(input("请输入第二个数字;"))
-if b == "+":
-   print(a + c)
-elif b == "-":
-    print(a - c)
-elif b == "*":
-    print(a * c)
-elif b == "/":
-    if c == 0:
-        print("除数不能为0")
+while True:
+    try:
+        a=int(input("请输入第一个数字："))
+    except ValueError:
+        print("请输入整数")
+        continue
+
+    b=input("请输入运算符号：")
+
+    try:
+        c=int(input("请输入第二个数字："))
+    except ValueError:
+        print("请输入整数")
+        continue
+        
+    if b == "+":
+      print(a + c)
+    elif b == "-":
+      print(a - c)
+    elif b == "*":
+      print(a * c)
+    elif b == "/":
+       if c == 0:
+          print("除数不能为0")
     else:
         print(a // c)
+else:
+    print("运算符号错误，请输入+,-,*,/")
